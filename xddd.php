82dd7ea562f645d87a7fffb1fdfe6d0d05184f8c
workingforyou <56809625+workingforyou@users.noreply.github.com>
2019-11-15 17:18:51 -0500

    Create xddd.php

---
+++ xddd.php
@@ -0,0 +1,32 @@
+<?php
+
+$usuario="root";
+$password=""; 
+$servidor="localhost";
+$basededatos="prueba4";
+
+$conexion= mysqli_connect($servidor,$usuario,"") or die ("Error con el servidor de la base de datos");
+
+$bd= mysqli_select_db($conexion,$basededatos) or die ("Error conexion al conectarse a la base de datos");
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$message=$_POST['message'];
+
+$sql="INSERT INTO datos4 VALUES('$name','$email','$phone', '$message')";
+
+$ejecutar=mysqli_query($conexion, $sql);
+
+if(!$ejecutar){
+    
+    echo "Hubo un error";
+    
+} else{
+    
+    echo "Datos Guardados correctamente";
+    
+}
+
+
+?>
